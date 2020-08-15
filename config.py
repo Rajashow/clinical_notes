a34fedf06d0448d9ddfd55a3b7a91f066d61e059
Rajashow <rajashowparajuli@gmail.com>
2020-08-14 21:42:38 -0400

    model path

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ MAX_LEN = 128
 TRAIN_BATCH_SIZE = 32
 VALID_BATCH_SIZE = 8
 EPOCHS = 10
-MODEL_PATH = "emilyalsentzer/Bio_ClinicalBERT"
+MODEL_PATH = "Bio_ClinicalBERT.mdl"
 TRAINING_FILE = "processed/ner/i2b2_train.csv"
 TOKENIZER = AutoTokenizer.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
