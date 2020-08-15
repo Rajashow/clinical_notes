d3ab4521765b870a44b10aff814d7d7e0b224e42
Rajashow <rajashowparajuli@gmail.com>
2020-08-14 23:00:13 -0400

    more tags

--- preprocess.py
+++ preprocess.py
@@ -7,20 +7,16 @@ import xml.etree.ElementTree as ET
 import numpy as np
 import re
 from tqdm import tqdm
+import config
 
 # %%
-OUTDIR = "./processed"
-TRAIN_DIRS = [
-    "training-RiskFactors-Gold-Set1/",
-]
-TEST_DIRS = ["testing-RiskFactors-Gold"]
-# ['MEDICATION', 'OBSEE', 'SMOKER', 'HYPERTENSION', 'PHI', 'FAMILY_HIST']
+OUTDIR = "processed/ner"
+TRAIN_DIR = "new_bert/train"
+TEST_DIR = "new_bert/test"
 TAGS = set(["MEDICATION", "OBSEE", "SMOKER", "HYPERTENSION", "PHI", "FAMILY_HIST"])
 TEXT_TAG = "TEXT"
 TAGS_TAG = "TAGS"
-START_CDATA = "<TEXT><![CDATA["
-END_CDATA = "]]></TEXT>"
-
+NUMBER_WORD_PER_SENT = config.MAX_LEN // 1.5
 # %%
 
 
@@ -52,25 +48,27 @@ def maxDisjointIntervals(itr: iter):
         return [[-1, -1, "O", ""]]
 
 
-def valid_label(tag):
-    return (
-        tag.tag in TAGS
-        and tag.attrib["start"] != "-1"
-        and tag.attrib["end"] != "-1"
-        and (
-            (tag.tag == "SMOKER" and tag.attrib["status"] != "unknown")
-            or tag.tag != "SMOKER"
-        )
-    )
+def has_valid_label_idx(tag):
+    return tag.tag in TAGS and tag.attrib["start"] != "-1" and tag.attrib["end"] != "-1"
 
 
-def get_tag_data(tag):
+def is_doc_lvl_tag(tag):
+    DOC_LVL_MODIFER = set(("not present", "unknown"))
+    return get_modifer(tag, leading_hyphen=False) in DOC_LVL_MODIFER
+
+
+def get_modifer(tag, leading_hyphen=True):
     modifer = tag.attrib.get(
         "status", tag.attrib.get("TYPE", tag.attrib.get("indicator", ""))
     ).replace(" ", "-")
-    if modifer:
+    if modifer and leading_hyphen:
         modifer = f"-{modifer}"
-    tag_name = f"{tag.tag}{modifer}"
+    return modifer.upper()
+
+
+def get_tag_data(tag):
+
+    tag_name = f"{tag.tag}{get_modifer(tag)}"
     return (
         int(tag.attrib["start"]),
         int(tag.attrib["end"]),
@@ -84,7 +82,11 @@ def process_xml(i, file):
 
     clinical_note = xml_parsed.find(TEXT_TAG).text
     tag_containers = xml_parsed.findall(TAGS_TAG)
-    ext_tags = [get_tag_data(tag) for tag in tag_containers[0] if valid_label(tag)]
+    ext_tags = [
+        get_tag_data(tag)
+        for tag in tag_containers[0]
+        if has_valid_label_idx(tag) and not is_doc_lvl_tag(tag)
+    ]
 
     interval_sub_set_ = maxDisjointIntervals(ext_tags)
     interval_idx = 0
@@ -117,24 +119,29 @@ def process_all_xml(folder=None, outdir="", out_modifer=""):
     xmls = glob(os.path.join(folder, "*.xml"))
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-    max_len = 0
     with open(os.path.join(outdir, f"i2b2{out_modifer}.csv"), "w") as file:
         csv_file = csv.writer(file,)
         csv_file.writerow(["filename", "sentence", "number", "word", "label"])
-        for i, file in tqdm(enumerate(xmls), desc=f"Processing files from {folder}: "):
+        for i, file in tqdm(
+            enumerate(xmls), total=len(xmls), desc=f"Processing files from {folder}: "
+        ):
             words, labels = process_xml(i, file)
-            max_len = max(max_len, len(words))
             csv_file.writerows(
                 [
-                    (os.path.basename(file), i // 100, i % 100, word, label)
+                    (
+                        os.path.basename(file),
+                        i // NUMBER_WORD_PER_SENT,
+                        i % NUMBER_WORD_PER_SENT,
+                        word,
+                        label,
+                    )
                     for i, (word, label) in enumerate(zip(words, labels))
                 ]
             )
-    print(max_len)
 
 
 # %%
-process_all_xml("new_bert/train", outdir="processed/ner", out_modifer="_train")
-process_all_xml("new_bert/test", outdir="processed/ner", out_modifer="_test")
+process_all_xml(TRAIN_DIR, outdir=OUTDIR, out_modifer="_train")
+process_all_xml(TEST_DIR, outdir=OUTDIR, out_modifer="_test")
 
 # %%
