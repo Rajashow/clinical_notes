44114f5d568fbcf2089291109bbae7a89734a776
Rajashow <rajashowparajuli@gmail.com>
2020-08-17 19:12:46 -0400

    added more metrics

--- test.py
+++ test.py
@@ -1,20 +1,23 @@
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics import confusion_matrix, classification_report, f1_score
 
 import joblib
 import torch
+from tqdm import tqdm
+import seaborn as sns
 
 import config
 import dataset
 from model import EntityModel
-from tqdm import tqdm
 from utils import process_data
-from sklearn.metrics import f1_score
-from sklearn.preprocessing import MultiLabelBinarizer
 
 if __name__ == "__main__":
 
     meta_data = joblib.load("meta.bin")
     enc_tag = meta_data["enc_tag"]
+    idx2class = list(enc_tag.classes_)
     num_tag = len(list(enc_tag.classes_))
     sentences, tags, enc_label_ = process_data(config.TESTING_FILE, enc_tag)
     dataset = dataset.EntityDataset(texts=sentences, tags=tags)
@@ -28,6 +31,8 @@ if __name__ == "__main__":
     model.to(device)
     final_loss = 0
     model.eval()
+    y_pred_list = []
+
     for data in tqdm(data_loader, total=len(data_loader)):
         for k, v in data.items():
             data[k] = v.to(device)
@@ -35,9 +40,17 @@ if __name__ == "__main__":
 
         y_pred = torch.argmax(tag, dim=-1).cpu().numpy()
         y_true = data["target_tag"].cpu().numpy()
-        m = MultiLabelBinarizer().fit(y_true)
+        y_pred_list.append(y_pred)
 
+        m = MultiLabelBinarizer().fit(y_true)
         final_loss += f1_score(
             m.transform(y_true), m.transform(y_pred), average="macro"
         )
+
     print(f"f1 score : { final_loss / len(data_loader):.5%}")
+    y_pred_list = [a.squeeze().tolist() for a in y_pred_list]
+    confusion_matrix_df = pd.DataFrame(confusion_matrix(tags, y_pred_list)).rename(
+        columns=idx2class, index=idx2class
+    )
+    print(classification_report(tags, y_pred_list))
+    sns.heatmap(confusion_matrix_df, annot=True)
