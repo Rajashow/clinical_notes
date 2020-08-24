3abc978e23d62e2a8d5ca245825169e0d2d77995
Rajashow <rajashowparajuli@gmail.com>
2020-08-24 12:00:02 -0400

    fixed sentence fragmentation

--- utils.py
+++ utils.py
@@ -23,18 +23,23 @@ def process_data(csv_path, enc_label=None, remove_idetincal_chained_values=False
         return (sentences,tags,label encod
         er)
     """
+
     GROUPBY = ["filename", "sentence"]
-    data = pd.read_csv(csv_path, keep_default_na=False)
+    data = pd.read_csv(config.TRAINING_FILE, keep_default_na=False)
     if not enc_label:
         enc_label = preprocessing.LabelEncoder()
 
     data.loc[:, "label"] = enc_label.fit_transform(data["label"])
     data = data.groupby(GROUPBY)
     if remove_idetincal_chained_values:
-        data = data.apply(
-            lambda group: group if len(group["label"].unique()) != 1 else None
-        ).dropna()
+        data = (
+            data.apply(
+                lambda group: group if len(group["label"].unique()) != 1 else None
+            )
+            .dropna()
+            .reset_index(drop=True)
+            .group_by(GROUPBY)
+        )
 
-    sentences = data["word"].values
-    tag = data["label"].values
-    return (sentences, tag, enc_label)
+    sentences = data["word"].apply(list).values
+    tag = data["label"].apply(list).values
